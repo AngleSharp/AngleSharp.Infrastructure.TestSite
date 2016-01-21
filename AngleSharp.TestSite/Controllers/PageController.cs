9afa996f22ac6ad0af513bae864a8c284be2c991
Florian Rappl <rappl.florian@gmail.com>
2016-01-21 16:03:30 +0100

    Added new static page for full resource loading

---
+++ AngleSharp.TestSite/Controllers/PageController.cs
@@ -0,0 +1,34 @@
+﻿namespace AngleSharpSubmitTest.Controllers
+{
+    using AngleSharpSubmitTest.Models;
+    using System.IO;
+    using System.Web.Mvc;
+
+    public class PageController : Controller
+    {
+        // Get: Page
+        [HttpGet]
+        public ViewResult Index()
+        {
+            var form = Request.Form;
+            var count = form.Count;
+            var reader = new StreamReader(Request.InputStream);
+            var fields = new FormEntryModel[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                fields[i] = new FormEntryModel
+                {
+                    Name = form.AllKeys[i],
+                    Value = form[i]
+                };
+            }
+
+            return View(new FormModel
+            {
+                Fields = fields,
+                Content = reader.ReadToEnd()
+            });
+        }
+    }
+}
